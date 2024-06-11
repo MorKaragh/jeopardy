eba5babe0fe3e3f1e88fa167940b9afd656fdfd3
Sergei <hentai.exe@gmail.com>
2024-06-11 23:32:37 +0400

    next

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import datetime
+import json
 import logging
 from pathlib import Path
+from typing import Optional
 
 import uvicorn
 from fastapi import FastAPI
 from starlette.requests import Request
+from starlette.responses import Response
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from starlette.websockets import WebSocket
@@ -21,9 +24,22 @@ templates = Jinja2Templates(directory="ui/templates")
 @app.get("/")
 async def root_page(request: Request):
     return templates.TemplateResponse(
-        name="root.html", context={"request": request}
+        name="root.html", context={"request": request, "room_name": "rname", "user_name": "uname"}
     )
 
+
+@app.get("/login")
+async def login_form(request: Request):
+    return templates.TemplateResponse(
+        name="login.html", context={"request": request}
+    )
+
+@app.post("/rooms")
+async def create_room(request: Request, response: Response):
+    print(request)
+    return templates.TemplateResponse(
+        name="game_room.html", context={"request": request}
+    )
 
 @app.get("/table")
 async def open_game_table(request: Request):
@@ -67,12 +83,31 @@ async def give_answer(request: Request,
     )
 
 
-@app.websocket("/ws")
+connections = {}
+
+
+@app.websocket("/game")
 async def websocket_endpoint(websocket: WebSocket):
+    connections[websocket] = {}
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        message = json.loads(data)
+        if message["msg_type"] == "room_hello":
+            connections[websocket]["user_name"] = message["user_name"]
+            connections[websocket]["room_name"] = message["room_name"]
+
+
+async def send_game_updates(user_name: str, room_name: str, event: dict):
+    websocket = find_socket(user_name, room_name)
+    await websocket.send_text(templates.get_template("game_control.html").render({"data": "data"}))
+
+
+def find_socket(user_name, room_name) -> Optional[WebSocket]:
+    for connection in connections:
+        if connections[connection]["user_name"] == user_name and connections[connection]["room_name"] == room_name:
+            return connection
+    return None
 
 
 if __name__ == '__main__':
